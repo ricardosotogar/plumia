fe7516a06ec6f8847c8537e46577aa9e28d0a5fc
rsotogar <Rsotogar17@gmail.com>
2026-04-13 17:58:59 +0200

    9.27

--- corrections-config.js
+++ corrections-config.js
@@ -5,8 +5,8 @@
 // ============================================================================
 (function() {
 window.PLUMIA = {};
-window.PLUMIA.CONFIG_VERSION = '9.26';
-console.log('📦 corrections-config.js v9.26 cargado');
+window.PLUMIA.CONFIG_VERSION = '9.27';
+console.log('📦 corrections-config.js v9.27 cargado');
 window.PLUMIA.COLOR_MAP = {
   1:{name:"Rojo",hex:"FF0000",type:"text"},
   2:{name:"Amarillo",hex:"FFD966",type:"highlight"},
